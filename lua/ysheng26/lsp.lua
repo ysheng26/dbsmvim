164b45abcee502aa7ecbcbc0865fa3f0cec7cb29
Yun Sheng <uewing@gmail.com>
2024-12-23 18:57:46 -0500

    lsp works

--- lua/ysheng26/lsp.lua
+++ lua/ysheng26/lsp.lua
@@ -1,4 +1,44 @@
 require('mason').setup()
-require('mason-lspconfig').setup()
 
--- require('lspconfig').rust_analyzer.setup {}
+
+-- require("conform").setup({
+--     formatters_by_ft = {}
+-- })
+
+-- local cmp = require('cmp')
+-- local cmp_lsp = require("cmp_nvim_lsp")
+-- local capabilities = vim.tbl_deep_extend(
+--     "force",
+--     {},
+--     vim.lsp.protocol.make_client_capabilities(),
+--     cmp_lsp.default_capabilities())
+
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "gopls",
+    },
+    handlers = {
+        function(server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {
+                -- capabilities = capabilities
+            }
+        end,
+
+        ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup {
+                -- capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        runtime = { version = "Lua 5.1" },
+                        diagnostics = {
+                            globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                        }
+                    }
+                }
+            }
+        end,
+    }
+})
