821120f611ae8c3419443da04c2f6f0b278bfa84
Yun Sheng <uewing@gmail.com>
2024-12-24 02:00:42 -0500

    enable treesitter text objects

--- lua/ysheng26/treesitter.lua
+++ lua/ysheng26/treesitter.lua
@@ -1,5 +1,23 @@
 require 'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = true
-    }
+    highlight = { enable = true },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner',
+                ['al'] = '@loop.outer',
+                ['il'] = '@loop.inner',
+                ['aa'] = '@parameter.outer',
+                ['ia'] = '@parameter.inner',
+                ['is'] = '@block.inner',
+                ['as'] = '@block.outer',
+            },
+        },
+
+    },
 }
