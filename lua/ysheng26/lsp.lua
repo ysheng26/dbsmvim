97f574c16773c305b1885f56640b37f7b8bc43e7
Yun Sheng <uewing@gmail.com>
2025-11-16 11:13:27 -0500

    key conflict between oil and lsp

--- lua/ysheng26/lsp.lua
+++ lua/ysheng26/lsp.lua
@@ -124,7 +124,7 @@ autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, opts)
 		vim.keymap.set("n", "<leader>D", vim.diagnostic.setqflist, opts)
 		vim.keymap.set("n", "cd", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "g.", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
 		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
