af585a1c0d90e854cca802c43c64dec4e58d624b
Yun Sheng <uewing@gmail.com>
2026-04-07 20:49:08 -0400

    update colors

--- lua/ysheng26/unokai.lua
+++ lua/ysheng26/unokai.lua
@@ -4,50 +4,58 @@ local function hi(group, options)
 end
 
 local function override()
-	-- 1. Popup / Float Windows (K Hover)
+	local cyan_fg = { fg = "#66d9ef", bold = false }
+	local green_fg = { fg = "#a6e22e", bold = false }
+	local red_fg = { fg = "#f92672", bold = false }
+	local white_fg = { fg = "#f8f8f2", bold = false }
+	local yellow_fg = { fg = "#e6db74", bold = false }
+
+	-- Popup / Float Windows (K Hover)
 	hi("NormalFloat", { bg = "#1e1e1e", fg = "#f8f8f2" })
 	hi("FloatBorder", { fg = "#ffffff", bg = "#1e1e1e" })
 	hi("FloatTitle", { fg = "#66d9ef", bg = "#1e1e1e", bold = false })
 
-	-- 2. Unokai Reverts: Types & Structures (Cyan & Flat)
-	hi("Type", { fg = "#66d9ef", bold = false })
-	hi("Structure", { fg = "#66d9ef", bold = false })
-	hi("@type", { fg = "#66d9ef", bold = false }) -- Modern Treesitter support
+	-- Unokai Reverts: Types & Structures (Cyan & Flat)
+	hi("Type", cyan_fg)
+	hi("Structure", cyan_fg)
+	hi("@type", cyan_fg) -- Modern Treesitter support
 
-	-- 3. Unokai Reverts: Functions (Green)
-	hi("Function", { fg = "#a6e22e" })
-	hi("@function", { fg = "#a6e22e" })
+	-- Unokai Reverts: Functions (Green)
+	hi("Function", green_fg)
+	hi("@function", green_fg)
 
-	-- 4. Identifiers (White/Classic)
-	hi("Identifier", { fg = "#f8f8f2" })
-	hi("@variable", { fg = "#f8f8f2" })
+	-- Identifiers (White/Classic)
+	hi("Identifier", white_fg)
+	hi("@variable", white_fg)
 
-	-- 5. Keywords & Statements (Pink & Flat)
-	local pink_flat = { fg = "#f92672", bold = false }
-	hi("Keyword", pink_flat)
-	hi("Statement", pink_flat)
-	hi("PreProc", pink_flat)
-	hi("Define", pink_flat)
-	hi("@keyword", pink_flat)
+	-- Keywords & Statements (Pink & Flat)
+	hi("Keyword", red_fg)
+	hi("Statement", red_fg)
+	hi("PreProc", red_fg)
+	hi("Define", red_fg)
+	hi("@keyword", red_fg)
 
-	-- 6. HTML / JSX / TSX: Attributes (Green)
-	local attr_green = { fg = "#a6e22e", bold = false }
-	hi("htmlArg", attr_green)
-	hi("tsxAttrib", attr_green)
-	hi("jsxAttrib", attr_green)
-	hi("xmlAttrib", attr_green)
-	hi("@tag.attribute", attr_green) -- Crucial for Treesitter
+	-- HTML / JSX / TSX: Attributes (Green)
+	hi("htmlArg", green_fg)
+	hi("tsxAttrib", green_fg)
+	hi("jsxAttrib", green_fg)
+	hi("xmlAttrib", green_fg)
+	hi("@tag.attribute", green_fg) -- Crucial for Treesitter
 
-	-- 7. HTML / JSX / TSX: Tags (Pink/Red)
-	local tag_pink = { fg = "#f92672", bold = false }
-	hi("Tag", tag_pink)
-	hi("htmlTagName", tag_pink)
-	hi("tsxTagName", tag_pink)
-	hi("jsxTagName", tag_pink)
-	hi("xmlTagName", tag_pink)
-	hi("@tag", tag_pink) -- Crucial for Treesitter
-	hi("@tag.builtin", tag_pink)
+	-- HTML / JSX / TSX: Tags (Pink/Red)
+	hi("Tag", red_fg)
+	hi("htmlTagName", red_fg)
+	hi("tsxTagName", red_fg)
+	hi("jsxTagName", red_fg)
+	hi("xmlTagName", red_fg)
+	hi("@tag", red_fg) -- Crucial for Treesitter
+	hi("@tag.builtin", red_fg)
 
+	-- yaml
+	hi("yamlBlockMappingKey", red_fg)
+	hi("yamlPlainScalar", yellow_fg)
+
+	-- treesitter context
 	hi("TreesitterContext", { bg = "#1e1e1e", fg = "#f8f8f2" })
 	hi("TreesitterContextBottom", { underline = true, sp = "Grey" })
 	hi("TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
